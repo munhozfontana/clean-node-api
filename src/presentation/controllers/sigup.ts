08ab9c09b2a1317cb35fed82eb96502f7e5da283
munhozfontana <munhozfontana@gmail.com>
2020-05-05 21:39:37 -0300

    refactor: make SignUpController retorms to a protocol

--- src/presentation/controllers/sigup.ts
+++ src/presentation/controllers/sigup.ts
@@ -1,8 +1,9 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../erros/missing-param-error'
 import { badRequest } from '../helper/http-helper'
+import { Controller } from '../helper/controller'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFileds = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFileds) {
