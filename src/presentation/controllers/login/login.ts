e0c2d4b229baa3cb9fdba84f40c1eabda0674877
munhozfontana <munhozfontana@gmail.com>
2020-05-09 18:05:34 -0300

    feat: ensere LoginCOntroller returns 400 if no password is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -4,6 +4,11 @@ import { MissingParamError } from '../../erros'
 
 export class LoginController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return await badRequest(new MissingParamError('email'))
+    if (!httpRequest.body.email) {
+      return await badRequest(new MissingParamError('email'))
+    }
+    if (!httpRequest.body.password) {
+      return await badRequest(new MissingParamError('password'))
+    }
   }
 }
