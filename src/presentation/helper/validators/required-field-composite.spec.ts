cba2309a63d1f1b9990c69bb67d3bfd316e91479
munhozfontana <munhozfontana@gmail.com>
2020-05-11 08:09:55 -0300

    test: ensure RequiredFieldValidation returns a MissingParamError if validation fails

---
+++ src/presentation/helper/validators/required-field-composite.spec.ts
@@ -0,0 +1,10 @@
+import { RequiredFieldValidation } from './required-field-composite'
+import { MissingParamError } from '../../erros'
+
+describe('Required Field Validation', () => {
+  test('should return a MissingParamError if validation fails', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ name: 'any_name' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+})
