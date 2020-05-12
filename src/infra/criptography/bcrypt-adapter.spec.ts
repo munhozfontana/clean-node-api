1f793b2539dd96804b06f9e2dc560ab56e6eeb6b
munhozfontana <munhozfontana@gmail.com>
2020-05-12 07:31:55 -0300

    test: ensure BcrytAdapter returns true when compare succeeds

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -31,7 +31,7 @@ describe('Bcrept Adapter', () => {
 
   test('Should throw if bvrypt throws', async () => {
     const sut = makeSut()
-    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    jest.spyOn(bcrypt, 'hash').mockRejectedValueOnce(new Error())
     const promise = sut.hash('any_value')
     await expect(promise).rejects.toThrow()
   })
@@ -41,5 +41,11 @@ describe('Bcrept Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'compare')
     await sut.compare('any_value', 'any_hash')
     expect(hashSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+  })
+
+  test('Should call true when compare succeeds', async () => {
+    const sut = makeSut()
+    const hash = await sut.compare('any_value', 'any_hash')
+    expect(hash).toBe(true)
   })
 })
